a1a5d765e9b580f9236e95e88a552d10ef520952
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-15 16:42:04 +0900

    (feat) MemberRepository 추가

---
+++ src/main/java/com/jun/accountmanager/member/dao/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.jun.accountmanager.member.dao;
+
+import com.jun.accountmanager.member.domain.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
