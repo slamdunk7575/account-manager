4600c6b0ac318432463e198b5826a93e4d1e7b26
slamdunk7575 <slamdunk7575@gmail.com>
2022-07-15 16:32:31 +0900

    (feat) JPA Audition 설정

---
+++ src/main/java/com/jun/accountmanager/config/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package com.jun.accountmanager.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
